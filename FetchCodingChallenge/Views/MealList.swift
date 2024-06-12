b72c81da04ce94257c1286698a751346904dbe87
SankalpKadam <sankalptp07@gmail.com>
2024-06-12 01:19:30 -0500

    Organized file structure and started working on the meal list view of the app.

---
+++ FetchCodingChallenge/Views/MealList.swift
@@ -0,0 +1,18 @@
+//
+//  MealList.swift
+//  FetchCodingChallenge
+//
+//  Created by Sankalp Kadam on 6/12/24.
+//
+
+import SwiftUI
+
+struct MealList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealList()
+}

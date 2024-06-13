264a6068b2d9e4612aabc411bee70f48113af8e0
SankalpKadam <sankalptp07@gmail.com>
2024-06-12 23:44:52 -0500

    Create a View Model for the Meal list.

--- FetchCodingChallenge/ViewModels/MealListItemViewModel.swift
+++ FetchCodingChallenge/ViewModels/MealListItemViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+final class MealListItemViewModel : ObservableObject {
+    
+    @Published var mealListItems : [MealListItem] = [MealListItem(id: "53409", itemTitle: "Apam Balik", itemImage: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg")]
+    
+    func getMeals(){
+        mealListItems.append(MealListItem(id: "53407", itemTitle: "Apam Balik", itemImage: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg"))
+        
+            mealListItems.append(MealListItem(id: "3409", itemTitle: "Apam Balik", itemImage: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg"))
+        
+            mealListItems.append(MealListItem(id: "409", itemTitle: "Apam Balik", itemImage: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg"))
+    }
+    
+}

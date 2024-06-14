9d3d799bdf776f3761dfd3368a4c88f3f46044da
SankalpKadam <67373254+SankalpKadam@users.noreply.github.com>
2024-06-14 11:16:11 -0500

    Implemented alphabetical sorting.

--- FetchCodingChallenge/ViewModels/MealDetailedViewModel.swift
+++ FetchCodingChallenge/ViewModels/MealDetailedViewModel.swift
@@ -1,99 +1,43 @@
 //
-//  MealDetailedViewModel.swift
+//  MealListItemViewModel.swift
 //  FetchCodingChallenge
 //
-//  Created by Sankalp Kadam on 6/13/24.
+//  Created by Sankalp Kadam on 6/12/24.
 //
 
 import Foundation
 
-final class MealDetailedViewModel: ObservableObject {
-    @Published var detailedMeal:[MealDetailedView] = []
-    @Published var ingredients:[String] = []
-    @Published var measures:[String] = []
+final class MealListItemViewModel : ObservableObject {
     
-    // Fetching all the details of a meal using its mealId
-    func getMealDetails(forMealId mealId:String) async throws -> MealDetailedView {
-        let endpoint = "https://themealdb.com/api/json/v1/1/lookup.php?i=\(mealId)"
+    @Published var mealListItems : [MealListItem] = []
+    
+    // This function makes a request to the API endpoint and stores fills the mealListItems array with the response received from the API
+    func getMeals() async throws{
+        let endpoint = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
         
         guard let url = URL(string: endpoint) else {
-            
-            print("error in \(endpoint)")
             throw APIErrors.errorInURL
         }
-        
         let (data, response) = try await URLSession.shared.data(from: url)
-        //Checking if response is OK
+        // Checking if response is Ok
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            print("erros in \(response)")
             throw APIErrors.errorInResponse
         }
-        
-        //Decoding the JSON to store it in the form of resepctive model
+        // Deconding to store the JSON response in the form of respective model
         do{
-            
             let decoder = JSONDecoder()
-            let moddedResponse = try decoder.decode(DetailedMeal.self, from: data)
-            detailedMeal = moddedResponse.meals
-            prepareTheIngredientsAndMeasuresList() //Filtering nil values for the ingredients and measures
-            return detailedMeal[0]
-            
-        }catch{
-            print("error in decoding")
+            let moddedResponse = try decoder.decode(Meals.self, from:data)
+            mealListItems = moddedResponse.meals.sorted{$0.strMeal < $1.strMeal} // sorts the meals in alphabetical order
+        }catch {
             throw APIErrors.errorInData
         }
+        
     }
     
+}
 
-    // Working with optionals and storing the values if provided otherwise empty string is added to the list
-    func prepareTheIngredientsAndMeasuresList(){
-        
-        ingredients = [
-            detailedMeal[0].strIngredient1,
-            detailedMeal[0].strIngredient2,
-            detailedMeal[0].strIngredient3,
-            detailedMeal[0].strIngredient4,
-            detailedMeal[0].strIngredient5,
-            detailedMeal[0].strIngredient6,
-            detailedMeal[0].strIngredient7,
-            detailedMeal[0].strIngredient8,
-            detailedMeal[0].strIngredient9,
-            detailedMeal[0].strIngredient10,
-            detailedMeal[0].strIngredient11,
-            detailedMeal[0].strIngredient12,
-            detailedMeal[0].strIngredient13,
-            detailedMeal[0].strIngredient14,
-            detailedMeal[0].strIngredient15,
-            detailedMeal[0].strIngredient16,
-            detailedMeal[0].strIngredient17,
-            detailedMeal[0].strIngredient18,
-            detailedMeal[0].strIngredient19,
-            detailedMeal[0].strIngredient20,
-        ].compactMap{ $0 }
-        
-        measures = [
-            detailedMeal[0].strMeasure1,
-            detailedMeal[0].strMeasure2,
-            detailedMeal[0].strMeasure3,
-            detailedMeal[0].strMeasure4,
-            detailedMeal[0].strMeasure5,
-            detailedMeal[0].strMeasure6,
-            detailedMeal[0].strMeasure7,
-            detailedMeal[0].strMeasure8,
-            detailedMeal[0].strMeasure9,
-            detailedMeal[0].strMeasure10,
-            detailedMeal[0].strMeasure11,
-            detailedMeal[0].strMeasure12,
-            detailedMeal[0].strMeasure13,
-            detailedMeal[0].strMeasure14,
-            detailedMeal[0].strMeasure15,
-            detailedMeal[0].strMeasure16,
-            detailedMeal[0].strMeasure17,
-            detailedMeal[0].strMeasure18,
-            detailedMeal[0].strMeasure19,
-            detailedMeal[0].strMeasure20,
-        ].compactMap{ $0 }
-
-    }
-    
+enum APIErrors: Error{
+    case errorInURL
+    case errorInResponse
+    case errorInData
 }

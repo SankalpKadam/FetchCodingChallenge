c196c90e5b5cda60e030131d5c538fd4ad84a114
SankalpKadam <67373254+SankalpKadam@users.noreply.github.com>
2024-06-14 11:13:14 -0500

    Creating a separate page to display the instructions.

---
+++ FetchCodingChallenge/Views/InstructionsView.swift
@@ -0,0 +1,25 @@
+//
+//  InstructionsView.swift
+//  FetchCodingChallenge
+//
+//  Created by Sankalp Kadam on 6/14/24.
+//
+
+import SwiftUI
+
+// Displays the instructions for the current meal
+struct InstructionsView: View {
+    var detailedInstructions: String
+    var modelObject: MealDetailedViewModel
+    var body: some View {
+        List{
+            Text(detailedInstructions)
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        .listStyle(PlainListStyle())
+    }
+}
+
+#Preview {
+    InstructionsView(detailedInstructions: "", modelObject: MealDetailedViewModel())
+}
